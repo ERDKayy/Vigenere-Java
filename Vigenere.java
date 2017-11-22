4d402d23ebefc51f42f9c36dff846abeaf40671c
Eric Kimball <eric-k93@live.com>
2017-11-21 21:27:24 -0800

    one error on compile, track down tomororw

--- Vigenere.java
+++ Vigenere.java
@@ -75,48 +75,49 @@ public class Vigenere {
 
     static String encryption(String text, final String key) {
         FileOutputStream fileOut = null;
-        boolean encrytped = false;
+        boolean encrypted = false;
         String encryptedText = "";
+        String encryptionKey = key;
 
-        while (!encrypted) {
-            if ((length(text) != length(key)) || length(key) < length(text)) {
-                int difference = length(text) - length(key);
-                for (int index = 0; index < diff; index ++) {
-                        key = key + key.charAt(index);
+        while (encrypted != true) {
+            if (text.length() != encryptionKey.length() && encryptionKey.length() < text.length()) {
+                int difference = text.length() - encryptionKey.length();
+                for (int index = 0; index < difference; index ++) {
+                    encryptionKey = encryptionKey + encryptionKey.charAt(index);
                 }
-
-                for (int i = 0; i < length(encryptedText); i++){
-                    char c = text.charAt(i);
-                    char k = key.charAt(i);
-                    int textCharInt = charToInt(c);
-                    int keyCharInt = charToInt(k);
-
-                    if ((textCharInt + keyCharInt) > 26) {
-                        int shiftDiff = (textCharInt + keyCharInt) - 26;
-                        encryptedText = encryptedText + intToChar(keyCharInt -1);
-                    }else{
-                        encryptedText = encryptedText + intToChar(textCharInt + keyCharInt);
-                    }
-                }
-                encrypted = true;
             }
+            for (int i = 0; i < encryptedText.length(); i++){
+                char c = text.charAt(i);
+                char k = encryptionKey.charAt(i);
+                int textCharInt = charToInt(c);
+                int keyCharInt = charToInt(k);
+
+                if ((textCharInt + keyCharInt) > 26) {
+                    int shiftDiff = (textCharInt + keyCharInt) - 26;
+                    encryptedText = encryptedText + intToChar(keyCharInt -1);
+                }else{
+                    encryptedText = encryptedText + intToChar(textCharInt + keyCharInt);
+                }
+            }
+                encrypted = true;
         }
         return encryptedText;
     }
 
-    public static decryption(String encryptedText, String key){
+    public static String decryption(String encryptedText, String key){
         boolean decrypted = false;
         String decryptedText = "";
-        while (!decrypted){
-            if (length(encryptedText) != lengthgth(key) || lengthgth(key) < lengthgth(encryptedText)){
-                int difference = length(encryptedText) - length(key);
+        String decryptionKey = key;
+        while (decrypted != true){
+            if ((encryptedText.length() != decryptionKey.length()) && (decryptionKey.length() < encryptedText.length())){
+                int difference = encryptedText.length() - decryptionKey.length();
                 for (int i = 0; i < difference; i++){
-                    key = key + key.chartAt(i);
+                    key = decryptionKey + decryptionKey.charAt(i);
                 }
             }
-            for (int i = 0; i < length(encryptedText); i++){
-                char c = text.charAt(i);
-                char k = key.charAt(i);
+            for (int i = 0; i < encryptedText.length(); i++){
+                char c = decryptionKey.charAt(i);
+                char k = decryptionKey.charAt(i);
                 int textCharInt = charToInt(c);
                 int keyCharInt = charToInt(k);
                 if (charToInt(c) - keyCharInt < 0){
@@ -134,29 +135,107 @@ public class Vigenere {
 
 
     public static void main(String []args) throws IOException {
-        System.out.println("Welcome to Vigenere cypher \n Menu:\n 1 - encryption\n 2 - decryption\n 3 - cryptanalysis (crypt text only");
         Scanner input = new Scanner(System.in);
-        exit = false;
+        String line = "";
+        boolean exit = false;
         int number;
-        while (exit != true){
+        System.out.println("Welcome to Vigenere cypher \n Menu:\n 1 - encryption\n 2 - decryption\n 3 - cryptanalysis (crypt text only\n 4 - exit");
+        while (exit != true) {
             try {
                 number = input.nextInt();
-            } catch (Exception e) {
+            }catch (Exception e) {
                 System.out.println("Your input must be one of the menu options!");
             }
             if (number == 1) {
-                String encryptedText = "";
-                System.out.println("Encryption.\n Please enter filengthame to be encrypted\n followed by the filengthame for the key...");
+                System.out.println("Enter file to be encrypted...");
                 String file = input.next();
-                String key = input.next();
-    
+                file += input.nextLine();
+                System.out.println("Enter the filename for the encryption key...");
+                String keyFile = input.next();
+                keyFile += input.nextLine();
+                System.out.println("Enter filename for encrypted message...");
+                String encryptedFile = input.next();
+                encryptedFile += input.nextLine();
+
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    BufferedReader br2 = new BufferedReader(new FileReader(keyFile));
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(encryptedFile));
+                    String key = br2.readLine();
+                    while ((line = br2.readLine()) != null){
+                        line = encryption(line, key);
+                        writer.write(line);
+                    }
+                    writer.close();
+                }catch(Exception e){
+                    System.out.println("file not found");
+                }
             }else if (number == 2) {
-    
+                System.out.println("Enter file to be decrypted...");
+                String encryptedFile = input.next();
+                encryptedFile += input.nextLine();
+                System.out.println("Enter filename for encryption key...");
+                String keyFile = input.next();
+                keyFile += input.nextLine();
+                System.out.println("Enter file for decrypted message...");
+                String file = input.next();
+                file += input.nextLine();
+
+                try {
+                    BufferedReader br = new BufferedReader(new FileReader(file));
+                    BufferedReader br2 = new BufferedReader(new FileReader(keyFile));
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(encryptedFile));
+                    String key = br2.readLine();
+                    while ((line = br.readLine()) != null){
+                        line = decryption(line, key);
+                        writer.write(line);
+                    }
+                    writer.close();
+                }catch(Exception e){
+                    System.out.println("File not found");
+                }
             }else if (number == 3) {
-    
+                System.out.println("Enter file for analysis...");
+                String file = input.next();
+                file += input.nextLine();
+                int[] coincidences = new int[40];
+
+                BufferedReader br = new BufferedReader(new FileReader(file));
+                String text ="";
+                String shifter ="";
+                shifter = br.readLine();
+                text = text + shifter;
+                while ((shifter = br.readLine()) != null) {
+                    text = text + " " + shifter;
+                }
+                String shifted = text;
+                for (int i = 1; i <= coincidences.length; i++){
+                    int coincidence = 0;
+                    shifted = shifted.substring(1);
+                    for (int j = 0; j < shifted.length(); j++){
+                        if (text.charAt(i) == shifted.charAt(i)){
+                            coincidence++;
+                        }
+                    }
+                    coincidences[i-1] = coincidence;
+                }
+
+                int top = coincidences[0];
+                int coincidencePeek = 0;
+                for (int index = 1; index < coincidences.length(); index++){
+                    if (coincidences[index] > top){
+                        top = coincidences[index];
+                        coincidencePeek = index;
+                    }
+                }
+                System.out.println("Length of key: " + (coincidencePeek + 1));
+            }else if (number == 4) {
+                exit = true;
             }else{
-                System.out.prinln("Your input must be one of the menu options!");
+                System.out.println("Your input must be one of the menu options!");
+                exit = true;
             }
+    
         }
     }
 }
